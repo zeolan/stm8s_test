febc01eb9468c7c38e1a65c0d20a35be8ba713d4
Oleksandr Zelenskyi <Oleksandr.Zelenskyi@t3worldwide.com>
2016-06-05 15:58:50 +0300

    Defines.h added

---
+++ defines.h
@@ -0,0 +1,27 @@
+/*
+*   Some useful defines for stm8s
+*/
+
+/*
+*   I/O ports
+*/
+
+#define PORTA     @0x5000
+#define PORTA_DIR @0x5002
+#define PORTA_CR1 @0x500d
+
+#define PORTB     @0x5005
+#define PORTB_DIR @0x5007
+
+#define PORTC     @0x500a
+#define PORTC_DIR @0x500c
+
+#define PORTD     @0x500f
+#define PORTD_DIR @0x5011
+#define PORTD_CR1 @0x5012
+#define PORTD_CR2 @0x5013
+
+/*
+*   Timers
+*/
+
